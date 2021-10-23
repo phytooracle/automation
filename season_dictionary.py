b9bbf541e5f5afd24d1fae21c1303218c11fe2ee
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-22 18:07:36 -0700

    new pipeline

--- season_dictionary.py
+++ season_dictionary.py
@@ -22,8 +22,8 @@ def season_dictionary():
                 },
                 'workflow_1': {
                     'commands': {
-                    ['jx2json main_workflow_phase1.jx -a bundle_list.json > main_workflow_phase1.json', 
-                    'makeflow -T wq --json main_workflow_phase1.json -a -N phytooracle_3d -M phytooracle_3d -r 3 -p 0 -dall -o dall.log $@']
+                    'jx2json main_workflow_phase1.jx -a bundle_list.json > main_workflow_phase1.json', 
+                    'makeflow -T wq --json main_workflow_phase1.json -a -N phytooracle_3d -M phytooracle_3d -r 3 -p 0 -dall -o dall.log $@'
                     },
                     'outputs': {
                         'pipeline_out': 'preprocessing_out',
