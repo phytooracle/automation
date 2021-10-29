14dca1aec35573a507e3a97d2ecbfb936cf7dc86
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-29 16:13:01 -0700

    v2

--- pipeline.py
+++ pipeline.py
@@ -462,7 +462,7 @@ def main():
                             get_transformation_file(os.path.join(level_1, scan_date))
 
                         if not os.path.isfile('bundle_list.json'):
-                            # get_bundle_dir(os.path.join(level_1, scan_date))
+                            get_bundle_dir(os.path.join(level_1, scan_date))
                             get_bundle_json(os.path.join(level_1, scan_date))
 
                     run_workflow_2(args.season, args.sensor, season_dict)
