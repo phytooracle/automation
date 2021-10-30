7c84bcd90d3f63caf1704a2b6263e56f3c483305
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-30 09:38:18 -0700

    v2

--- pipeline.py
+++ pipeline.py
@@ -297,6 +297,7 @@ def download_level_1_data(irods_path):
     direc = irods_path.split('/')[-1]
     print(file_name)
     cmd1 = f'iget -rfPVT {irods_path}'
+    # cmd1 = f'iget -fKPVT {irods_path}'
     cwd = os.getcwd()
 
     if '.gz' in file_name: 
