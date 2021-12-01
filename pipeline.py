36ce9ca728cb739188aa71bdf1699eaefe559c87
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2021-12-01 13:23:28 -0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -610,9 +610,9 @@ def main():
                     update_process_one(os.getcwd()+'/')
                     pipeline_prep('individual_plants_out', bundle_size=args.bundle_size, plant=True)
 
-                    if not os.path.isdir(os.path.join(cwd, season_dict[args.season][args.sensor]['workflow_3']['outputs']['pipeline_out'], 'combined_pointclouds')):
-                        run_workflow_3(args.season, args.sensor, season_dict)
-                        uncompress_inference(cwd)
+                    #if not os.path.isdir(os.path.join(cwd, season_dict[args.season][args.sensor]['workflow_3']['outputs']['pipeline_out'], 'combined_pointclouds')):
+                    run_workflow_3(args.season, args.sensor, season_dict)
+                    uncompress_inference(cwd)
 
                     processing_dir = os.path.join(cwd, season_dict[args.season][args.sensor]['workflow_3']['outputs']['pipeline_out'], 'combined_pointclouds')
 
