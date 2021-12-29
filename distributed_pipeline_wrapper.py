574c047547a641d178ea86a5b92ed79bd9e86bf3
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-29 13:27:53 -0700

    change downloading

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -110,6 +110,22 @@ def download_cctools(cctools_version = '7.1.12', architecture = 'x86_64', sys_os
 
 
 # --------------------------------------------------
+def get_irods_path(dictionary, date):
+    """
+    Get IRODS path to download
+    
+    Input: 
+        - dictionary: Dictionary generated from the YAML file
+    
+    Output: 
+        - irods_path: CyVerse filepath
+    """
+    irods_path = os.path.join(dictionary['paths']['cyverse']['input']['basename'],\
+        ''.join([dictionary['paths']['cyverse']['input']['prefix'], date, dictionary['paths']['cyverse']['input']['suffix']]))
+    return irods_path
+
+
+# --------------------------------------------------
 def download_raw_data(irods_path):
     """Download raw dataset from CyVerse DataStore
     
@@ -719,53 +735,55 @@ def main():
 
         clean_directory()
         
-        
         with open(args.yaml, 'r') as stream:
             try:
                 global dictionary
                 dictionary = yaml.safe_load(stream)
-                build_containers(dictionary)
+                # build_containers(dictionary)
 
             except yaml.YAMLError as exc:
                 print(exc)
                 
-            kill_workers(dictionary['workload_manager']['job_name'])
-            if dictionary['tags']['sensor']=='scanner3DTop':
-                get_required_files_3d(dictionary=dictionary, date=date)
+            # kill_workers(dictionary['workload_manager']['job_name'])
+            irods_path = get_irods_path(dictionary, date)
+            download_raw_data(irods_path)
             
-            if args.hpc:
-                launch_workers(account=dictionary['workload_manager']['account'], 
-                        partition=dictionary['workload_manager']['partition'], 
-                        job_name=dictionary['workload_manager']['job_name'], 
-                        nodes=dictionary['workload_manager']['nodes'], 
-                        number_tasks=dictionary['workload_manager']['number_tasks'], 
-                        number_tasks_per_node=dictionary['workload_manager']['number_tasks_per_node'], 
-                        time=dictionary['workload_manager']['time_minutes'], 
+            # # if dictionary['tags']['sensor']=='scanner3DTop':
+            # #     get_required_files_3d(dictionary=dictionary, date=date)
+            
+            # if args.hpc:
+            #     launch_workers(account=dictionary['workload_manager']['account'], 
+            #             partition=dictionary['workload_manager']['partition'], 
+            #             job_name=dictionary['workload_manager']['job_name'], 
+            #             nodes=dictionary['workload_manager']['nodes'], 
+            #             number_tasks=dictionary['workload_manager']['number_tasks'], 
+            #             number_tasks_per_node=dictionary['workload_manager']['number_tasks_per_node'], 
+            #             time=dictionary['workload_manager']['time_minutes'], 
 
-                        mem_per_cpu=dictionary['workload_manager']['mem_per_cpu'], 
-                        manager_name=dictionary['workload_manager']['manager_name'], 
-                        min_worker=dictionary['workload_manager']['min_worker'], 
-                        max_worker=dictionary['workload_manager']['max_worker'], 
-                        cores=dictionary['workload_manager']['cores_per_worker'], 
-                        worker_timeout=dictionary['workload_manager']['worker_timeout_seconds'])
+            #             mem_per_cpu=dictionary['workload_manager']['mem_per_cpu'], 
+            #             manager_name=dictionary['workload_manager']['manager_name'], 
+            #             min_worker=dictionary['workload_manager']['min_worker'], 
+            #             max_worker=dictionary['workload_manager']['max_worker'], 
+            #             cores=dictionary['workload_manager']['cores_per_worker'], 
+            #             worker_timeout=dictionary['workload_manager']['worker_timeout_seconds'])
 
-            global seg_model_name, det_model_name
-            seg_model_name, det_model_name = get_model_files(args.seg_model, args.det_model)
+            # global seg_model_name, det_model_name
+            # seg_model_name, det_model_name = get_model_files(args.seg_model, args.det_model)
 
-            for k, v in dictionary['modules'].items():
+            # for k, v in dictionary['modules'].items():
                 
-                dir_name = os.path.join(*v['input_dir'])
-                files_list = get_file_list(dir_name, level=v['file_level'], match_string=v['input_file'])
-                write_file_list(files_list)
-                json_out_path = generate_makeflow_json(level=v['file_level'], files_list=files_list, command=v['command'], container=v['container']['simg_name'], inputs=v['inputs'], outputs=v['outputs'], date=date, sensor=dictionary['tags']['sensor'], json_out_path=f'wf_file_{k}.json')
-                run_jx2json(json_out_path, cctools_path, batch_type=v['distribution_level'], manager_name=dictionary['workload_manager']['manager_name'], retries=dictionary['workload_manager']['retries'], port=dictionary['workload_manager']['port'], out_log=True)
-                clean_directory()
+            #     dir_name = os.path.join(*v['input_dir'])
+            #     files_list = get_file_list(dir_name, level=v['file_level'], match_string=v['input_file'])
+            #     write_file_list(files_list)
+            #     json_out_path = generate_makeflow_json(level=v['file_level'], files_list=files_list, command=v['command'], container=v['container']['simg_name'], inputs=v['inputs'], outputs=v['outputs'], date=date, sensor=dictionary['tags']['sensor'], json_out_path=f'wf_file_{k}.json')
+            # #     run_jx2json(json_out_path, cctools_path, batch_type=v['distribution_level'], manager_name=dictionary['workload_manager']['manager_name'], retries=dictionary['workload_manager']['retries'], port=dictionary['workload_manager']['port'], out_log=True)
+            # #     clean_directory()
         
-            kill_workers(dictionary['workload_manager']['job_name'])
-            tar_outputs(date, dictionary)
-            create_pipeline_logs(date)
-            upload_outputs(date, dictionary)
-            clean_inputs(date)        
+            # # kill_workers(dictionary['workload_manager']['job_name'])
+            # # tar_outputs(date, dictionary)
+            # # create_pipeline_logs(date)
+            # # upload_outputs(date, dictionary)
+            # # clean_inputs(date)        
 
 
 # --------------------------------------------------
