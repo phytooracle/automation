281906a1b103aba2a5f06e15b61d3215a79df4ca
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-31 12:35:33 -0700

    cleaning

--- deprecated/create_fake_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-touch bundle_list.json
-touch main_workflow_phase1.json
-touch main_workflow_phase1.json.makeflowlog
-touch main_workflow_phase1.json.wqlog
-touch main_workflow_phase1.json.wqlog.tr
