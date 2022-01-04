df0f02453f8ee61234df594d48714050767e4d31
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-01-04 08:20:46 -0700

    refactor

--- mf_monitor.sh
+++ mf_monitor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-module load python/2.7/2.7.14
 export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
-${HOME}/cctools-7.1.12-x86_64-centos7/bin/makeflow_monitor wf_file_${1}.json.makeflowlog
+makeflow_monitor wf_file_${1}.json.makeflowlog
