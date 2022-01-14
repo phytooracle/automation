3f7d49eb0fd46e275ac66c30a78265584973f5ca
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-01-14 10:45:43 -0700

    documentation update

---
+++ slurm_submission.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --account=dukepauli
+#SBATCH --partition=standard
+#SBATCH --job-name="phytooracle_manager"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=94
+#SBATCH --time=96:00:00
+PIPE_COMMAND=${1}
+$PIPE_COMMAND
