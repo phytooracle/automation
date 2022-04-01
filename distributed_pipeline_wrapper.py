cb308cff16de343af34bf74584b0fd64df64f455
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-04-01 01:31:16 -0700

    changes

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -475,9 +475,13 @@ def launch_workers(cctools_path, account, partition, job_name, nodes, number_tas
         # fh.writelines(f"work_queue_factory -T local -M {manager_name} -w {min_worker} -W {max_worker} --cores {cores} -t {worker_timeout}\n")
         # fh.writelines(f"srun work_queue_worker -M {manager_name} --cores {cores} -t {worker_timeout}\n")
 
-    os.system(f"sbatch {outfile}")
-    os.system(f"ocelote 'sbatch {outfile}' '&& puma'")
-    os.system(f"elgato 'sbatch {outfile}' '&& puma'")
+    sp.call(f"sbatch {outfile}", shell=True)
+    sp.call(f"ocelote 'sbatch {outfile}' '&& puma'", shell=True)
+    sp.call(f"elgato 'sbatch {outfile}' '&& puma'", shell=True)
+
+    # os.system(f"sbatch {outfile}")
+    # os.system(f"ocelote 'sbatch {outfile}' '&& puma'")
+    # os.system(f"elgato 'sbatch {outfile}' '&& puma'")
 
 
 # --------------------------------------------------
