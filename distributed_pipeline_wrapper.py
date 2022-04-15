e2ed245ca3678b6886fb9f5caa7cdaabc64cefc0
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-04-14 21:10:20 -0700

    Update distributed_pipeline_wrapper.py

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -204,8 +204,10 @@ def download_raw_data(irods_path):
     # will take forever.  So we do this instead...
     import shlex
     print(f"Examining {tarball_filename}, this can take a second...")
+    gzip_extensions = ['.tgz', '.tar.gz']
     
-    if ['.tgz', '.tar.gz'] in tarball_filename:
+    if any(x in tarball_filename for x in gzip_extensions):
+#     if gzip_extension in tarball_filename:
         command = f"tar -ztf {tarball_filename}"
     else:
         command = f"tar -tf {tarball_filename}"
