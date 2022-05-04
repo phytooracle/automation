2be83c2173c999540b359c2a78e1c5d70bcac2a4
Travis-Simmons <920117874@student.ccga.edu>
2022-05-04 11:54:08 -0400

    added cctools building from source on non centos machines

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -16,6 +16,7 @@ import shutil
 import glob
 import tarfile
 import multiprocessing
+import platform
 
 
 # --------------------------------------------------
@@ -113,33 +114,70 @@ def download_cctools(cctools_version = '7.1.12', architecture = 'x86_64', sys_os
     '''
     cwd = os.getcwd()
     home = os.path.expanduser('~')
-    
-    cctools_file = '-'.join(['cctools', cctools_version, architecture, sys_os])
-    
-    if not os.path.isdir(os.path.join(home, cctools_file)):
-        print(f'Downloading {cctools_file}.')
-        cctools_url = ''.join(['http://ccl.cse.nd.edu/software/files/', cctools_file])
-        cmd1 = f'cd {home} && wget {cctools_url}.tar.gz'
-        sp.call(cmd1, shell=True)
-        os.chdir(home)
-        file = tarfile.open(f'{cctools_file}.tar.gz')
-        file.extractall('.')
-        file.close()
-        os.remove(f'{cctools_file}.tar.gz')
 
-        try:
-            shutil.move('-'.join(['cctools', cctools_version, architecture, sys_os+'.tar.gz', 'dir']), '-'.join(['cctools', cctools_version, architecture, sys_os]))
 
-        except:
-            pass
 
-        os.chdir(cwd)
-        print(f'Download complete. CCTools version {cctools_version} is ready!')
+    # builds from latest stable source if not on a centos system
+    if platform.linux_distribution()[0] != 'CentOS Linux':
+        cctools_file = 'cctools-stable-source'
+        if not os.path.isdir(os.path.join(home, 'cctools')):
+            print(f'Downloading {cctools_file}.')
+            cctools_url = ''.join(['http://ccl.cse.nd.edu/software/files/', cctools_file])
+            print(cctools_url)
+            cmd1 = f'cd {home} && wget {cctools_url}.tar.gz'
+            sp.call(cmd1, shell=True)
+            os.chdir(home)
+            cmd2 = f'mkdir non_centos_cctools && tar xzvf {cctools_file}.tar.gz -C non_centos_cctools'
+            sp.call(cmd2, shell = True)
+            os.chdir('non_centos_cctools')
 
+            print('Building cctools from source...')
+
+            os.chdir(glob.glob('./*')[0])
+
+            cmd3 = './configure --prefix $HOME/cctools && make && make install'
+            sp.call(cmd3, shell = True)
+
+
+
+            os.remove(os.path.join(home, f'{cctools_file}.tar.gz'))
+            
+            print(f'Download complete. CCTools is ready!')
+
+        else:
+            print('Required CCTools version already exists.')
+
+        return 'cctools/'
+
+    # builds from centos specific package if on centos
     else:
-        print('Required CCTools version already exists.')
 
-    return '-'.join(['cctools', cctools_version, architecture, sys_os])
+        cctools_file = '-'.join(['cctools', cctools_version, architecture, sys_os])
+    
+        if not os.path.isdir(os.path.join(home, cctools_file)):
+            print(f'Downloading {cctools_file}.')
+            cctools_url = ''.join(['http://ccl.cse.nd.edu/software/files/', cctools_file])
+            cmd1 = f'cd {home} && wget {cctools_url}.tar.gz'
+            sp.call(cmd1, shell=True)
+            os.chdir(home)
+            file = tarfile.open(f'{cctools_file}.tar.gz')
+            file.extractall('.')
+            file.close()
+            os.remove(f'{cctools_file}.tar.gz')
+
+            try:
+                shutil.move('-'.join(['cctools', cctools_version, architecture, sys_os+'.tar.gz', 'dir']), '-'.join(['cctools', cctools_version, architecture, sys_os]))
+
+            except:
+                pass
+
+            os.chdir(cwd)
+            print(f'Download complete. CCTools version {cctools_version} is ready!')
+
+        else:
+            print('Required CCTools version already exists.')
+
+        return '-'.join(['cctools', cctools_version, architecture, sys_os])
 
 
 # --------------------------------------------------
@@ -981,7 +1019,8 @@ def main():
                 return
                 
             irods_path = get_irods_input_path(dictionary, date, args)
-            dir_name = download_raw_data(irods_path)
+            # dir_name = download_raw_data(irods_path)
+            dir_name = 'ps2Top'
 
             #if dictionary['tags']['sensor']=='scanner3DTop':
                 #get_required_files_3d(dictionary=dictionary, date=date)
