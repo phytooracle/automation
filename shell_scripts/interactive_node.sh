a7a390fe0229be9f3b0fb46aa0de35ac3fbd2eed
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-08 19:38:45 -0700

    update manager

--- shell_scripts/interactive_node.sh
+++ shell_scripts/interactive_node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-salloc --nodes=1 --mem=470GB --ntasks=94 --time=168:00:00 --job-name=phytooracle_manager --account=frost_lab --partition=standard
+salloc --nodes=1 --mem-per-cpu=5GB --ntasks=2 --time=168:00:00 --job-name=phytooracle_manager --account=ericlyons --partition=high_priority --qos=user_qos_ericlyons
