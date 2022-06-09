18c2b509339a0b743b2f2298c08b0d87e9a55b52
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-09 00:21:06 -0700

    update slurm sub

--- shell_scripts/slurm_submission.sh
+++ shell_scripts/slurm_submission.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -l
-#SBATCH --account=<hpc account here>
-#SBATCH --partition=<windfall or standard>
+#SBATCH --account=ericlyons
+#SBATCH --partition=high_priority
+#SBATCH --qos=user_qos_ericlyons
 #SBATCH --job-name="phytooracle_manager"
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=94
-#SBATCH --time=96:00:00
-DATE=${1}
-YAML=${2}
-./distributed_pipeline_wrapper.py -hpc -d ${DATE} -y ${YAML}
+#SBATCH --ntasks=2
+#SBATCH --mem-per-cpu=5GB
+#SBATCH --time=168:00:00
+#DATE=${1}
+#YAML=${2}
+
+YAML=${1}
+./distributed_pipeline_wrapper.py -hpc -y ${YAML}
