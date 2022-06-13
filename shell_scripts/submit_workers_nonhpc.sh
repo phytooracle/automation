65341ebf7ba1a47578ff119b2d519818621eaddd
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-13 13:21:14 -0700

    Update submit_workers_nonhpc.sh

--- shell_scripts/submit_workers_nonhpc.sh
+++ shell_scripts/submit_workers_nonhpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CCTOOLS_HOME=${HOME}/cctools
 export PATH=${CCTOOLS_HOME}/bin:$PATH
-work_queue_factory -T local -M phytooracle_manager --memory 150000
+work_queue_factory -T local -M phytooracle_manager --memory 130000 -w 5 -W 10
