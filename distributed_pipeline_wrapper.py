8e7ca6144f3d0e693837d2485c8a6d45611209f3
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-06-17 15:20:05 -0700

    remove hard coded plant report move

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -776,18 +776,18 @@ def tar_outputs(scan_date, dictionary):
             if not os.path.isdir(_full_v):
                 print(f"Skipping the tarring of '{_full_v}' from yaml paths:outpath_subdirs because it was not found")
                 continue
-            if v == 'plant_reports':
-                # This is a hack created by Nathan.
-                src_dir = os.path.join(cwd, outdir, v)
-                dest_dir = os.path.join(cwd, scan_date, outdir, v)
-                print(f"Copying plant_reports")
-                shutil.copytree(src_dir, dest_dir)
-            else:
-                file_path = os.path.join(cwd, scan_date, outdir, f'{scan_date}_{v}.tar') 
-                print(f'Creating {file_path}.')
-                if not os.path.isfile(file_path):
-                    with tarfile.open(file_path, 'w') as tar:
-                        tar.add(v, recursive=True)
+            # if v == 'plant_reports':
+            #     # This is a hack created by Nathan.
+            #     src_dir = os.path.join(cwd, outdir, v)
+            #     dest_dir = os.path.join(cwd, scan_date, outdir, v)
+            #     print(f"Copying plant_reports")
+            #     shutil.copytree(src_dir, dest_dir)
+            # else:
+            file_path = os.path.join(cwd, scan_date, outdir, f'{scan_date}_{v}.tar') 
+            print(f'Creating {file_path}.')
+            if not os.path.isfile(file_path):
+                with tarfile.open(file_path, 'w') as tar:
+                    tar.add(v, recursive=True)
 
     os.chdir(cwd)
 
