d85fc97cbe4e8e4fde0149a7bc20fb16e0603ee2
Emmanuel Gonzalez <emmanuelgonzalez@gpu06.cyverse.org>
2022-06-21 11:35:12 -0700

    revert to default manager name

--- shell_scripts/submit_workers_nonhpc.sh
+++ shell_scripts/submit_workers_nonhpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CCTOOLS_HOME=${HOME}/cctools
 export PATH=${CCTOOLS_HOME}/bin:$PATH
-work_queue_factory -T local -M phytooracle_manager_rna_seq --memory 5000 -w 100 -W 200
+work_queue_factory -T local -M phytooracle_manager --memory 5000 -w 100 -W 200
