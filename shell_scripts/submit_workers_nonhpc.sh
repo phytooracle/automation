a2d6b6c369eaa88e9c7355324ce8bfa839105ec7
Emmanuel Gonzalez <emmanuelgonzalez@gpu06.cyverse.org>
2022-06-21 11:14:28 -0700

    update for rnaseq

--- shell_scripts/submit_workers_nonhpc.sh
+++ shell_scripts/submit_workers_nonhpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CCTOOLS_HOME=${HOME}/cctools
 export PATH=${CCTOOLS_HOME}/bin:$PATH
-work_queue_factory -T local -M phytooracle_manager --memory 130000 -w 5 -W 10
+work_queue_factory -T local -M phytooracle_manager_rna_seq --memory 5000 -w 100 -W 200
