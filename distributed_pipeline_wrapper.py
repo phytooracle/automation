2e963d8872866184bfc2b35e59043ec627de801b
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-23 14:22:03 -0700

    changes to jx

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -622,15 +622,15 @@ def generate_makeflow_json(cctools_path, level, files_list, command, container, 
     files_list = [file.replace('-west.ply', '').replace('-east.ply', '').replace('-merged.ply', '').replace('__Top-heading-west_0.ply', '') for file in files_list]
     timeout = 'timeout 1h '
     cwd = os.getcwd()
-    seg_model_name, det_model_name = get_model_files(dictionary['paths']['models']['segmentation'], dictionary['paths']['models']['detection'])
+    # seg_model_name, det_model_name = get_model_files(dictionary['paths']['models']['segmentation'], dictionary['paths']['models']['detection'])
 
-    if args.shared_file_system:
-        container = os.path.join(cwd, container)
-        seg_model_name = os.path.join(cwd, seg_model_name)
-        det_model_name = os.path.join(cwd, det_model_name)
-        inputs = [os.path.join(cwd, item) for item in inputs]
-        files_list = [os.path.join(cwd, item) for item in files_list]
-        print(container, seg_model_name, det_model_name)
+    # if args.shared_file_system:
+    #     container = os.path.join(cwd, container)
+    #     seg_model_name = os.path.join(cwd, seg_model_name)
+    #     det_model_name = os.path.join(cwd, det_model_name)
+    #     inputs = [os.path.join(cwd, item) for item in inputs]
+    #     files_list = [os.path.join(cwd, item) for item in files_list]
+    #     print(container, seg_model_name, det_model_name)
 
     if inputs:
         if sensor=='scanner3DTop':
