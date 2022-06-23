d5f82c3774557080d2627bf18b1cd73245e28a8c
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-23 06:53:57 -0700

    updated jx paths

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -623,6 +623,12 @@ def generate_makeflow_json(cctools_path, level, files_list, command, container, 
     timeout = 'timeout 1h '
     cwd = os.getcwd()
 
+    if args.shared_file_system:
+        container = os.path.join(cwd, container)
+        seg_model_name = os.path.join(cwd, seg_model_name)
+        det_model_name = os.path.join(cwd, det_model_name)
+        input = [os.path.join(cwd, item) for item in input]
+
     if inputs:
         if sensor=='scanner3DTop':
             
