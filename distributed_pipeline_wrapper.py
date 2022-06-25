a6ff073e5913ff8bbcb20c69e7436dcc40e0f181
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-06-25 01:37:36 -0700

    remove all inputs

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -715,10 +715,12 @@ def generate_makeflow_json(cctools_path, level, files_list, command, container, 
                         {
                                 "command" : timeout + command.replace('${FILE}', file).replace('${PLANT_PATH}', os.path.dirname(file)).replace('${SEG_MODEL_PATH}', seg_model_name).replace('${PLANT_NAME}', os.path.basename(os.path.dirname(file))).replace('${DET_MODEL_PATH}', det_model_name).replace('${SUBDIR}', os.path.basename(os.path.dirname(file))).replace('${DATE}', date),
                                 "outputs" : [out.replace('$PLANT_NAME', os.path.basename(os.path.dirname(file))).replace('$SUBDIR', os.path.join(os.path.basename(os.path.dirname(file)), os.path.basename(file))).replace('${DATE}', date).replace('$BASENAME', os.path.basename(os.path.dirname(file))) for out in outputs],
-                                "inputs"  : [file, 
-                                            container, 
-                                            seg_model_name, 
-                                            det_model_name]
+                                "inputs"  : [seg_model_name,
+                                             det_model_name]
+                                            #[file, 
+                                            #container, 
+                                            #seg_model_name, 
+                                            #det_model_name]
 
                         } for file in  files_list
                     ]
