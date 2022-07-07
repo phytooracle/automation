3c6b42972715809bcb98f84094a892fa257a287f
equant <nathanhendler@gmail.com>
2022-07-07 10:39:17 -0700

    merge

--- shell_scripts/mf_monitor.sh
+++ shell_scripts/mf_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CCTOOLS_HOME=${HOME}/cctools-7.4.2-x86_64-centos7
+export CCTOOLS_HOME=${HOME}/cctools/
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 makeflow_monitor wf_file_${1}.json.makeflowlog
