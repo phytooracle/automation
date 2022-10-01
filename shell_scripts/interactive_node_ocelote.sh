7e72eaae64f7c7a978702ce37cf88e0c484a45d6
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-10-01 16:04:52 -0700

    Update interactive_node_ocelote.sh

--- shell_scripts/interactive_node_ocelote.sh
+++ shell_scripts/interactive_node_ocelote.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-salloc --nodes=1 --mem-per-cpu=6gb --ntasks=6 --time=168:00:00 --job-name=phytooracle_manager --account=dukepauli --partition=standard
+salloc --nodes=1 --mem-per-cpu=6gb --ntasks=28 --time=168:00:00 --job-name=phytooracle_manager --account=dukepauli --partition=standard
