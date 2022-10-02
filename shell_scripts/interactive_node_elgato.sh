c7ddff89e80a1469d764355ebcd078723682251b
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-10-01 22:10:08 -0700

    Update interactive_node_elgato.sh

--- shell_scripts/interactive_node_elgato.sh
+++ shell_scripts/interactive_node_elgato.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-salloc --nodes=1 --mem-per-cpu=4gb --ntasks=6 --time=168:00:00 --job-name=phytooracle_manager --account=frost_lab --partition=standard
+salloc --nodes=1 --mem-per-cpu=6gb --ntasks=2 --time=168:00:00 --job-name=phytooracle_manager --account=frost_lab --partition=standard
