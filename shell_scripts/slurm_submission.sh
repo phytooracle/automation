0d7b27420b1354c91b981b8e2db6991eebbdb619
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-10-13 17:49:21 -0700

    update for 3d

--- shell_scripts/slurm_submission.sh
+++ shell_scripts/slurm_submission.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=user_qos_ericlyons
 #SBATCH --job-name="phytooracle_manager"
 #SBATCH --nodes=1
-#SBATCH --ntasks=2
+#SBATCH --ntasks=12
 #SBATCH --mem-per-cpu=5GB
 #SBATCH --time=168:00:00
 #DATE=${1}
